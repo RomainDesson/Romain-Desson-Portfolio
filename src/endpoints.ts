a6efc240c6d7aa55b0f07d5cbc46f2856b446833
romaindesson <romain.desson.02320@gmail.com>
2023-12-07 07:45:03 +0100

    Modify resume endpoint

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,2 +1,2 @@
 export const sendGridEndpoint = 'http://34.38.81.122:3000/sendmail'
-export const downloadResumeEndpoint = 'https://storage.cloud.google.com/romain-desson-resume/Romain%20Desson%20Profile.pdf'
+export const downloadResumeEndpoint = 'https://storage.googleapis.com/romain-resume/Romain%20Desson%20Profile.pdf'
