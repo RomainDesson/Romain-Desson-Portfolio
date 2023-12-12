b17ab21432b2a75d730bf685089190ea93c7328c
romaindesson <romain.desson.02320@gmail.com>
2023-12-12 08:49:32 +0100

    Modify email endpoint

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,2 +1,2 @@
-export const sendGridEndpoint = 'https://nodejs-typescript-sendgrid-2tq6ej2mn-romaindesson.vercel.app/sendmail'
+export const sendGridEndpoint = 'https://nodejs-typescript-sendgrid-g3lxqwxcy-romaindesson.vercel.app/api/sendmail'
 export const downloadResumeEndpoint = 'https://storage.googleapis.com/romain-resume/Romain%20Desson%20Profile.pdf'
